ec885407c212f00fcf0cb594a961281f5126d7bb
divyarustgi2003 <divyarustgi@gmail.com>
2024-09-17 00:27:04 +0530

    creating product endpoint

---
+++ drizzle/0001_broad_jackal.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "products" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" varchar(100) NOT NULL,
+	"image" text,
+	"description" text,
+	"price" integer NOT NULL,
+	"category" varchar,
+	"updated_at" timestamp DEFAULT CURRENT_TIMESTAMP,
+	"created_at" timestamp DEFAULT CURRENT_TIMESTAMP
+);
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,37 @@
+import { db } from "@/lib/db/db";
+import { products } from "@/lib/db/schema";
+import { productSchema } from "@/lib/validators/productSchema";
+import { writeFile } from "node:fs/promises";
+import path from "node:path";
+
+
+export async function POST(request: Request){
+    const data = await request.formData();
+    let validatedData;
+    try{
+        validatedData = productSchema.parse({
+            name:data.get('name'),
+            description: data.get('description'),
+            price: Number(data.get('price')),
+            image: data.get('image')
+        });
+    } catch(err){
+        return Response.json({message: err}, {status: 400});
+    }
+    const filename = `${Date.now()}.${validatedData.image.name.split(".").slice(-1)}`;
+
+    try{
+        const buffer = Buffer.from(await validatedData.image.arrayBuffer());
+        await writeFile(path.join(process.cwd(), "public/assets", filename), buffer);
+    }catch(err){
+        return Response.json({message: "Failed to save the file to fs"}, {status: 500});
+    }
+
+    try{
+        await db.insert(products).values({...validatedData, image: filename})
+    } catch(err){
+        return Response.json({message: "failed to store product into the database"}, {status: 500});
+    }
+
+    return Response.json({message: "Product created successfully"}, {status: 201});
+}
